af6ce1afe43afffc43b2bdae5b8d77673fcacbbe
Scott Nelson <scttnlsn94@gmail.com>
2014-10-22 15:45:42 -0700

    Fix for DSOUND issue

--- Dev/StaticSheepNeoBuildSystem/game/SheepGraphics/include/SheepGraphics/precompiled.h
+++ Dev/StaticSheepNeoBuildSystem/game/SheepGraphics/include/SheepGraphics/precompiled.h
@@ -8,8 +8,6 @@
 
 #include "Utilities.h"
 
-#include "dxerr.h"
-
 #include "DirectXIncludes.h"
 #include "WindowsIncludes.h"
 
@@ -19,7 +17,10 @@
 
 
 #if defined(_DEBUG)
-#define DXVerify(exp) if(FAILED(exp)){ErrorIf(true, "sheep_grahics", "DX Error %s", DXGetErrorString(exp));}
+#define DXVerify(exp) if(FAILED(exp)){const DWORD size = 100+1; WCHAR buffer[size]; \
+					  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,NULL, \
+					  exp,MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),(LPTSTR)buffer,size,NULL); \
+					  ErrorIf(true, "sheep_grahics", "DX Error %s", buffer);}
 #else
 #define DXVerify(x) (x)
 #endif
