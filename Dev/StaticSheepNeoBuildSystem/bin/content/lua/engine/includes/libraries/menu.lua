89072851df7dcfea1b3d7a33e439d219eb02da69
Zachary Nawar <remscar@live.com>
2014-11-20 22:31:39 -0800

    Support for more than one menu

--- Dev/StaticSheepNeoBuildSystem/bin/content/lua/engine/includes/libraries/menu.lua
+++ Dev/StaticSheepNeoBuildSystem/bin/content/lua/engine/includes/libraries/menu.lua
@@ -1,83 +1,115 @@
 if not menu then
   menu = {}
-  menu.buttons = {}
-  menu.selected = 0
-  menu.numButtons = 0
-  
-  menu.verticalStep = 1
-  menu.horizontalStep = 1
+  menu.list = {}
+  menu.meta = {}
 end
 
-
-
-function menu.Create(vStep, hStep)
+function menu.Create(vStep, hStep, gamepads)
   -- Initialize button table
   print("Created menu")
-  menu.buttons = {}
 
-  menu.numButtons = 0
-  menu.selected = 0
-  menu.verticalStep = vStep or 1
-  menu.horizontalStep = hStep or 1
+  local newMenu = {}
+  newMenu.buttons = {}
+
+  newMenu.numButtons = 0
+  newMenu.selected = 0
+  newMenu.verticalStep = vStep or 1
+  newMenu.horizontalStep = hStep or 1
+  newMenu.validGamePads = gamepads or {}
+
+  table.merge(menu.meta, newMenu)
+
+  table.insert(menu.list, newMenu)
+
+  newMenu.menuID = #menu.list
+
+  return newMenu
 end
 
-function menu.Delete()
-  print("Deleted menu")
-  menu.buttons = {}
+function menu.Update()
+  for id, realMenu in pairs(menu.list) do
+    realMenu:Update()
+  end
 end
 
-function menu.RegisterButton(btn)
+function menu.Delete(realMenu)
+  local id = realMenu.menuID
+  
+  print("Deleted menu: "..id)
+
+  menu.list[id]:Delete()
+
+  table.remove(menu.list, id)
+end
+
+
+
+
+function menu.meta:SetHStep(step)
+  self.horizontalStep = step
+end
+
+function menu.meta:SetVStep(step)
+  self.verticalStep = step
+end
+
+function menu.meta:Delete()
+  self.buttons = {}
+end
+
+function menu.meta:RegisterButton(btn)
   -- Insert button into menu
-  table.insert(menu.buttons, btn)
+  table.insert(self.buttons, btn)
 
-  menu.numButtons = menu.numButtons + 1
+  self.numButtons = self.numButtons + 1
 
   -- Tell our button about its menu index
-  btn.menuIndex = menu.numButtons
+  btn.menuBtnID = self.numButtons
+  btn.menuID = self.menuID
 
   -- If that was the first button to be added set it to hovered
-  if menu.numButtons == 1
-    menu.buttons[1]:SetHovered(true)
+  if self.numButtons == 1
+    self.buttons[1]:SetHovered(true)
   end
 
   print("Registered button into menu at index: "..#menu.buttons)
 end
 
-function menu.InsertButton(btn, index)
+function menu.meta:InsertButton(btn, index)
   -- Insert our button at the index in the menu
-  table.insert(menu.buttons, index, btn)
+  table.insert(self.buttons, index, btn)
 
   btn.menuIndex = index
 
-  menu.numButtons = menu.numButtons + 1
+  self.numButtons = self.numButtons + 1
 
   -- It happened that we inserted a button in the place or before
   -- the currently selected button, we should update that
-  if menu.selected >= index then
-    menu.selected = menu.selected + 1
+  if self.selected >= index then
+    self.selected = self.selected + 1
   end
 
   print("Registered button into menu at index: "..index)
 end
 
-function menu.RemoveButton(btn, index)
+function menu.meta:RemoveButton(btn, index)
   -- Take out the removed button
-  table.remove(menu.button, index)
+  table.remove(self.button, index)
 
-  menu.numButtons = menu.numButtons - 1
+  self.numButtons = self.numButtons - 1
 
-  if menu.selected == index then
-    menu.selected = menu.selected - 1
-    menu.button[menu.selected]:SetHovered(true)
-  elseif menu.selected > index then
-    menu.selected = menu.selected - 1
+  if self.selected == index then
+    self.selected = self.selected - 1
+    self.button[self.selected]:SetHovered(true)
+  elseif self.selected > index then
+    self.selected = self.selected - 1
   end
 
   print("Removed button from menu at index: "..index)
 end
 
-function menu.FindButtonIndex(btn)
-  for index, button in pairs(menu.buttons) do
+function menu.meta:FindButtonIndex(btn)
+  for index, button in pairs(self.buttons) do
     if button == btn then
       return index
     end
@@ -86,58 +118,58 @@ function menu.FindButtonIndex(btn)
   return -1
 end
 
-function menu.Update()
+function menu.meta:Update()
 
   local clicked = false
   local hMove = 0
   local vMove = 0
 
-  local newSelection = menu.selected
+  local newSelection = self.selected
 
   if hMove ~= 0 then
-    hMove = hMove * menu.horizontalStep
+    hMove = hMove * self.horizontalStep
     newSelection = newSelection + hMove
 
     -- Wrap around if we went under 1 or above max buttons
     if newSelection < 1 then
-      newSelection = menu.numButtons + newSelection
+      newSelection = self.numButtons + newSelection
     end
 
-    if newSelection > menu.numButtons then
-      newSelection = newSelection - menu.numButtons
+    if newSelection > self.numButtons then
+      newSelection = newSelection - self.numButtons
     end
 
     -- Tell our old selection to no longer be selected
-    menu.buttons[menu.selected]:SetHovered(false)
+    self.buttons[self.selected]:SetHovered(false)
 
     -- Update our current selection
-    menu.selected = newSelection
-    menu.buttons[menu.selected]:SetHovered(true)
+    self.selected = newSelection
+    self.buttons[self.selected]:SetHovered(true)
   end
 
   if vMove ~= 0 then
-    vMove = vMove * menu.verticalStep
+    vMove = vMove * self.verticalStep
     newSelection = newSelection + vMove
 
     -- Wrap around if we went under 1 or above max buttons
     if newSelection < 1 then
-      newSelection = menu.numButtons + newSelection
+      newSelection = self.numButtons + newSelection
     end
 
-    if newSelection > menu.numButtons then
-      newSelection = newSelection - menu.numButtons
+    if newSelection > self.numButtons then
+      newSelection = newSelection - self.numButtons
     end
 
     -- Tell our old selection to no longer be selected
-    menu.buttons[menu.selected]:SetHovered(false)
+    self.buttons[self.selected]:SetHovered(false)
 
     -- Update our current selection
-    menu.selected = newSelection
-    menu.buttons[menu.selected]:SetHovered(true)
+    self.selected = newSelection
+    self.buttons[self.selected]:SetHovered(true)
   end
 
   if clicked then
-    menu.buttons[menu.selected]:Press()
+    self.buttons[self.selected]:Press()
   end
 
 end
