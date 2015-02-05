573910eadba26c097432bdfc7e865742ddd26d02
Zachary Nawar <remscar@live.com>
2015-02-05 13:51:27 -0800

    Final Intro Screen

--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/sound/CSoundPlayer.h
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/sound/CSoundPlayer.h
@@ -23,9 +23,11 @@ namespace Framework
     
     // Plays the requested sound with the parameters in instance. instance is defaulted if not provided
     void Play(const std::string &name, SoundInstance* instance = &SoundInstance());
+    void LuaPlay(std::string name);
     
     // Stops the requested sound. FadeOut does nothing currently.
     void Stop(std::string name, FadeOut mode);
+    void LuaStop(std::string name);
 
     // Pauses the requested sound. Pause = true, Unpause = false
     void Pause(std::string name, bool flag);
