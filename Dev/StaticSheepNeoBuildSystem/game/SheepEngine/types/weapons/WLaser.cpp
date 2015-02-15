87ee4047632d4a3b0638c706ca2054b48ff44db2
Jon Sourbeer <chronosnoj@gmail.com>
2015-02-14 16:52:07 -0800

    testing lasers

--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/types/weapons/WLaser.cpp
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/types/weapons/WLaser.cpp
@@ -4,15 +4,17 @@
 #include "Components/colliders/CBoxCollider.h"
 #include "Components/controllers/player/CPlayerController.h"
 #include "components/gameplay_scripts/CBullet_default.h"
+#include "Matrix3D.h"
+
 
 namespace Framework
 {
   Laser::Laser()
   {
-    delay = 0;
-    damage = 20;
-    knockback = 60;
-    semi = true;
+    delay = 0.0f;
+    damage = 1;
+    knockback = 0;
+    semi = false;
   }
 
   Laser::~Laser()
@@ -25,6 +27,12 @@ namespace Framework
     Transform *playerTrans = player->GetComponent <Transform>(eTransform);
     Vec3 AimDir = player->GetComponent<PlayerController>(ePlayerController)->aimDir;
     
+    /*if (AimDir.x < 0.0f)
+      arrowTransform->SetRotation((float)atan(aimDir.y / aimDir.x));
+    else
+      arrowTransform->SetRotation((float)atan(aimDir.y / aimDir.x) + PI);*/
+
+
     Handle playerCollider = player->GetComponentHandle(eBoxCollider);
     BoxCollider *bc = player->space->GetHandles().GetAs<BoxCollider>(playerCollider);
     bc->SetRayCast(playerTrans->GetTranslation(), AimDir, player->archetype);
@@ -37,7 +45,9 @@ namespace Framework
 
   void Laser::DelayUpdate(float dt)
   {
-
+    delay -= dt;
+    if (delay < -100)
+      delay = 0;
   }
 
   void Laser::ResetDelay()
