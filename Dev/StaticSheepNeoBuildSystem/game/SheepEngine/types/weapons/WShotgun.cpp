001d9d18593eaddf10c41915dbcde8df10431034
Zachary Nawar <remscar@live.com>
2015-04-02 19:28:51 -0700

    I am gun that shot.

--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/types/weapons/WShotgun.cpp
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/types/weapons/WShotgun.cpp
@@ -64,6 +64,8 @@ namespace Framework
     //middle shot - first shot
     bullet = CreateBullet(player, "Bullet_shot");
 
+    float centerTheta = atan2f(AimDir.y, AimDir.x) - (PI / 2.0f);
+
       //subsequent shots
     for (int i = 0; i < 4; ++i)
     {
@@ -79,7 +81,7 @@ namespace Framework
         bullet = CreateBullet(player, "Bullet_shot");
 
         float theta = atan2f(bulletDir.y, bulletDir.x) - (PI / 2.0f);
-        Mat3D rotation(theta);
+        Mat3D rotation(centerTheta - theta);
         ParticleSystem* part = bullet->GetComponent<ParticleSystem>(eParticleSystem);
         part->direction.m_startMin = rotation * part->direction.m_startMin;
         part->direction.m_startMax = rotation * part->direction.m_startMax;
