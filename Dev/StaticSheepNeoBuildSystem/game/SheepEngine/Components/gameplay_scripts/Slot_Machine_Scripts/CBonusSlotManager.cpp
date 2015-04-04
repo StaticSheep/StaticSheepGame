9eb9a6aa6683c06c7c6008b9bc3cab6ea3f800d9
Immutus <chronosnoj@gmail.com>
2015-04-04 11:16:40 -0700

    working on bonus

--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.cpp
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.cpp
@@ -10,13 +10,48 @@ namespace Framework
     space->hooks.Add("RoundOver", self, BUILD_FUNCTION(CBonusSlotManager::RoundOver));
 
     //still to be updated
-    space->hooks.Add("GivePlayerCoins", self, BUILD_FUNCTION(Level1_Logic::GivePlayerCoins));
+   // space->hooks.Add("GivePlayerCoins", self, BUILD_FUNCTION(Level1_Logic::GivePlayerCoins));
 
 
     //player->space->hooks.Call("GivePlayerCoins", playerController->playerNum, coinAmount);
+
+      //initialize important starting values
+    m_currentLead = BONUSMAX;
+    m_currentLeader = NOLEADER;    //no current leader, no value awarded
+
   }
 
+  void CBonusSlotManager::RoundStart()
+  {
+    for (int i = 0; i < 4; ++i)
+    {
+      m_stats[i].stats.coins = 0;
+      m_stats[i].stats.deaths = 0;
+      m_stats[i].stats.kills = 0;
+    }
 
+  }
+
+  void CBonusSlotManager::RoundOver()
+  {
+      //determine winner
+    for (int i = 0; i < 4; ++i)
+    {
+      if (m_stats[i].cdk[m_coinDeathKill] < m_currentLead)
+      {
+        m_currentLead = m_stats[i].cdk[m_coinDeathKill];
+        m_currentLeader = i;
+        m_tie = false;
+      }
+      else if (m_stats[i].cdk[m_coinDeathKill] == m_currentLead)
+      {
+        m_tie = true;
+      }
+    }
+
+      //dispatch prize
+
+  }
 
 
 }
--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.h
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.h
@@ -9,19 +9,23 @@
 
 namespace Framework
 {
+  #define BONUSMAX	2147483647
+  #define NOLEADER 4
 
   enum BONUSTYPES
   {
     COINS,
-    KILLS,
     DEATHS,
+    KILLS,
     TYPES
   };
 
+    //MOST is set as negative one to make one 'if' check work for determining winner
+    //MOST will result in a more negative number, which means it will work against BONUSMAX
   enum BONUSMODIFIERS
   {
-    MOST,
-    LEAST,
+    MOST = -1,
+    LEAST = 1,
     MODIFIERS
   };
 
@@ -40,18 +44,40 @@ namespace Framework
       //messaged functions
     void RoundStart();
     void RoundOver();
-    void GivePlayerCoins(int player, int coins);
+    //void GivePlayerCoins(int player, int coins);
 
 
     private:
-      struct PlayerBonusStats
+
+      struct bonusStats
       {
         int coins;
         int deaths;
         int kills;
       };
 
-      PlayerBonusStats stats[4];
+      struct PlayerBonusStats
+      {
+        union
+        {
+          int cdk[3];
+          bonusStats stats;
+        };
+        
+      };
+
+      PlayerBonusStats m_stats[4];
+      BONUSTYPES m_type;
+      BONUSMODIFIERS  m_mod;
+
+      bool m_tie;                 //ties are not allowed - this tells whether a tie is occurring
+      int m_currentLeader;          //current leader of the bonus
+      int m_currentLead;            //current lead value
+
+        //used to make determing how to determine winner simpler
+      int m_coinDeathKill;          //tracks which part of array to use
+
+      
 
   };
 
