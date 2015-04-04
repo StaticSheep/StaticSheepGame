bb2d9157fb9d7d78a9b1fd970a2590ee87ebeefe
Jon Sourbeer <chronosnoj@gmail.com>
2015-04-04 12:44:22 -0700

    finishing slot integration for bonus slots, then will test

--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.cpp
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.cpp
@@ -8,28 +8,62 @@ namespace Framework
   {
     space->hooks.Add("RoundStart", self, BUILD_FUNCTION(CBonusSlotManager::RoundStart));
     space->hooks.Add("RoundOver", self, BUILD_FUNCTION(CBonusSlotManager::RoundOver));
-
-    //still to be updated
-   // space->hooks.Add("GivePlayerCoins", self, BUILD_FUNCTION(Level1_Logic::GivePlayerCoins));
-
-
-    //player->space->hooks.Call("GivePlayerCoins", playerController->playerNum, coinAmount);
-
+    space->hooks.Add("GivePlayerCoins", self, BUILD_FUNCTION(CBonusSlotManager::GivePlayerCoins));
+    space->hooks.Add("PlayerDied", self, BUILD_FUNCTION(CBonusSlotManager::PlayerDied));//m_playerNum, m_killer);
+    
       //initialize important starting values
     m_currentLead = BONUSMAX;
     m_currentLeader = NOLEADER;    //no current leader, no value awarded
 
   }
 
+  void CBonusSlotManager::Remove()
+  {
+    space->hooks.Remove("RoundStart", self);
+    space->hooks.Remove("RoundOver", self);
+    space->hooks.Remove("GivePlayerCoins", self);
+    space->hooks.Remove("PlayerDied", self);
+
+  }
+
+  void CBonusSlotManager::BonusSlotStart()
+  {
+
+  }
+
+  void CBonusSlotManager::BonusSlotResults()
+  {
+
+  }
+
+  void CBonusSlotManager::BonusSlotPrize()
+  {
+    //tie resulted - dispatch message for tie and no winner, or perhaps there should be a winner?
+    if (m_tie)
+      return;
+
+    switch (m_prize)
+    {
+    case COINS:
+      space->hooks.Call("GivePlayerCoins", m_currentLeader, m_prizeSize * 1000);
+      break;
+
+    case CHIP:
+      ChipController *CC = space->GetGameObject(owner)->GetComponent<ChipController>(eChipController);
+      CC->GivePlayerChip(m_currentLeader, m_prizeSize);
+      break;
+    }
+  }
+
   void CBonusSlotManager::RoundStart()
   {
+      //iterate through all players
     for (int i = 0; i < 4; ++i)
     {
       m_stats[i].stats.coins = 0;
       m_stats[i].stats.deaths = 0;
       m_stats[i].stats.kills = 0;
     }
-
   }
 
   void CBonusSlotManager::RoundOver()
@@ -50,8 +84,17 @@ namespace Framework
     }
 
       //dispatch prize
-
+    BonusSlotPrize();
   }
 
+  void CBonusSlotManager::GivePlayerCoins(int playerNum, int coins)
+  {
+    m_stats[playerNum].stats.coins += coins;
+  }
 
+  void CBonusSlotManager::PlayerDied(int playerNum, int killer)
+  {
+    ++m_stats[playerNum].stats.deaths;
+    ++m_stats[killer].stats.kills;
+  }
 }
--- Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.h
+++ Dev/StaticSheepNeoBuildSystem/game/SheepEngine/Components/gameplay_scripts/Slot_Machine_Scripts/CBonusSlotManager.h
@@ -12,25 +12,39 @@ namespace Framework
   #define BONUSMAX	2147483647
   #define NOLEADER 4
 
-  enum BONUSTYPES
+  class CBonusSlotManager: public GameComponent
   {
-    COINS,
-    DEATHS,
-    KILLS,
-    TYPES
-  };
+    enum BONUSTYPES
+    {
+      COINS,
+      DEATHS,
+      KILLS,
+      TYPES
+    };
 
     //MOST is set as negative one to make one 'if' check work for determining winner
     //MOST will result in a more negative number, which means it will work against BONUSMAX
-  enum BONUSMODIFIERS
-  {
-    MOST = -1,
-    LEAST = 1,
-    MODIFIERS
-  };
+    enum BONUSMODIFIERS
+    {
+      MOST = -1,
+      LEAST = 1,
+      MODIFIERS
+    };
 
-  class CBonusSlotManager: public GameComponent
-  {
+    enum BONUSPRIZE
+    {
+      COINS,
+      CHIP,
+      BONUSPRIZESIZE
+    };
+
+    enum BONUSPRIZEMOD
+    {
+      SMALL = 1,
+      MED = 2,
+      LARGE = 5,
+    };
+
     CBonusSlotManager();
     ~CBonusSlotManager();
 
@@ -39,12 +53,13 @@ namespace Framework
 
     void BonusSlotStart();
     void BonusSlotResults();
-
+    void BonusSlotPrize();
 
       //messaged functions
     void RoundStart();
     void RoundOver();
-    //void GivePlayerCoins(int player, int coins);
+    void GivePlayerCoins(int player, int coins);
+    void PlayerDied(int playerNum, int killer);
 
 
     private:
@@ -69,12 +84,14 @@ namespace Framework
       PlayerBonusStats m_stats[4];
       BONUSTYPES m_type;
       BONUSMODIFIERS  m_mod;
+      BONUSPRIZE m_prize;
+      BONUSPRIZEMOD m_prizeSize;
 
       bool m_tie;                 //ties are not allowed - this tells whether a tie is occurring
       int m_currentLeader;          //current leader of the bonus
       int m_currentLead;            //current lead value
 
-        //used to make determing how to determine winner simpler
+        //used to make determing winner simpler
       int m_coinDeathKill;          //tracks which part of array to use
 
       
